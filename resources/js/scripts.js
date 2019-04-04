7336ecd597506261b6d248fb24f2fc302a3bc7fd
Simon <simonhodson72@gmail.com>
2019-04-04 18:58:57 +0100

    Init commit of existing repo

---
+++ resources/js/scripts.js
@@ -0,0 +1,45 @@
+/*global $,document*/ 
+
+$(document).ready(function () {
+     /* Sticky Navigation*/
+    $('.js--section-features').waypoint(function (direction) {
+        
+        if (direction === "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+   }, {
+       offset: '60px;'
+   });
+    
+
+    
+    /* Scroll on button click*/
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+});
+
+/*
+    $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+    });
+
+    */
